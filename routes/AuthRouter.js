f29c13fdce816c09b6e102a25c0429cdc25672f7
Unknown <yuchiehchen@outlook.com>
2018-10-19 21:39:23 +0800

    帳密比對實作

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,4 +1,18 @@
+const _ = require('lodash');
 const express = require('express');
+
+async function verifyUser(data) {
+    const username = _.get(data, 'username'); // 從 data 取出 username
+    const password = _.get(data, 'password'); // 從 data 取出 username
+
+    if(username === 'billy' && password === '1234') { // pass
+        return Promise.resolve({
+            username,
+            email: 'billy@gmail.com',
+        });
+    }
+    return Promise.reject(new Error('Fail'));
+}
 
 /**
  * 
